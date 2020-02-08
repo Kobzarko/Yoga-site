e479b6e328e8dc4134f3648f8193f1777b8bf431
ypushiev <pyshievgeeks@gmail.com>
2020-02-08 23:43:01 +0200

    modified js/script.js

--- js/script.js
+++ js/script.js
@@ -76,20 +76,36 @@ function setClock(id , endtime){
 	let timer = document.getElementById('timer'),
 		hours = timer.querySelector('.hours'),
 		minutes = timer.querySelector('.minutes'),
-		seconds = timer.querySelector('.seconds');
+		seconds = timer.querySelector('.seconds'),
 		timeinterval = setInterval(updateClock,1000);
+
+
 		// МЕНЯЕМ текст. значение в тегах с классами
 		function updateClock(){
 			let t = getTimeRemaining(endtime);
-			hours.textContent = t.hours;
-			minutes.textContent = t.minutes;
-			seconds.textContent = t.seconds;
+
+		function addZero(num){
+			if (num<=9) {
+				return "0"+ num;
+			}
+			else{
+				return num;
+			}
+		}	
+			hours.textContent = addZero(t.hours);
+			minutes.textContent = addZero(t.minutes);
+			seconds.textContent = addZero(t.seconds);
+
+
 
 			if (t.total<=0) {
-				clearInterval(timeinterval);
+				clearInterval(timer.timeinterval);
+				hours.textContent = '00';
+				minutes.textContent = '00';
+				seconds.textContent = '00';
 			}
 		}
 	}
 
-	setClock('timer', deadline);
+	setClock('timer', deadLine);
 });
