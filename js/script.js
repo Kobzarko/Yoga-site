875a17a44453054a1e7ec8fe17064081e2ef2cd2
ypushiev <pyshievgeeks@gmail.com>
2020-02-15 18:43:54 +0200

    add modal window

--- js/script.js
+++ js/script.js
@@ -44,68 +44,124 @@ window.addEventListener('DOMContentLoaded', function(){
 
 	// Timer
 
-	let deadLine = '2020-02-02';
+	// конечная дата
+    let deadline = new Date(2020, 2, 25);
+    // считает разницу между датами
+    function getTimeRemaining(endtime) {
+        let t = Date.parse(endtime) - Date.parse(new Date()),
+            seconds = Math.floor((t / 1000) % 60),
+            minutes = Math.floor(((t / 1000) / 60) % 60),
+            hours = Math.floor((t / 1000 / 60 / 60) % 24),
+            days = Math.floor((t / (1000 * 60 * 60 * 24)));
 
-// получаем данные о времени
-function getTimeRemaining(endtime){
-	// вычисляем разницу между датами
-	// находим количество миллисекунд в промежутке
-	let t = Date.parse(endtime) - Date.parse(new Date()),
-	//количество секунд
-	seconds = Math.floor((t/1000)%60),
-	// количество минут
-	minutes = Math.floor((t/60000)%60),
-	// количество часов
-	hours = Math.floor(t/(1000*60*60));
-		// РЕЗУЛЬТАТ 
-		return{
-			'total':t,
-			'hours':hours,
-			'minutes': minutes,
-			'seconds': seconds
-		};
-
-	// // количество часов
-	// 	hours = Math.floor((t/(1000*60*60)%24);
-	// // количество дней	
-	// 	days = Math.floor((t/(1000*60*60*24)));
-}
-
-// функция для изменения статической верстки
-function setClock(id , endtime){
-	let timer = document.getElementById('timer'),
-		hours = timer.querySelector('.hours'),
-		minutes = timer.querySelector('.minutes'),
-		seconds = timer.querySelector('.seconds'),
-		timeinterval = setInterval(updateClock,1000);
+        // hours = Math.floor((t / 1000) / 3600),
 
 
-		// МЕНЯЕМ текст. значение в тегах с классами
-		function updateClock(){
-			let t = getTimeRemaining(endtime);
+        //возвращаем в виде объекта
+        return {
+            'total': t,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds,
+            'days': days
+        };
 
-		function addZero(num){
-			if (num<=9) {
-				return "0"+ num;
-			}
-			else{
-				return num;
-			}
-		}	
-			hours.textContent = addZero(t.hours);
-			minutes.textContent = addZero(t.minutes);
-			seconds.textContent = addZero(t.seconds);
+    }
+
+    // функция будет изменять статическую верстку на динмаическую
+    function setClock(id, endtime) {
+        let timer = document.getElementById(id),
+            hours = timer.querySelector('.hours'),
+            minutes = timer.querySelector('.minutes'),
+            seconds = timer.querySelector('.seconds'),
+            days = timer.querySelector('.days'),
+            word = timer.querySelector('.word'),
+            timeInterval = setInterval(updateClock, 1000); // обновляет данные каждую секунду
 
 
 
-			if (t.total<=0) {
-				clearInterval(timer.timeinterval);
-				hours.textContent = '00';
-				minutes.textContent = '00';
-				seconds.textContent = '00';
-			}
-		}
-	}
 
-	setClock('timer', deadLine);
+        // обновляет данные в выбранных полях
+        function updateClock() {
+            let t = getTimeRemaining(endtime);
+
+            function addZero(num) {
+                if (num <= 9) {
+                    return "0" + num;
+                } else {
+                    return num;
+                }
+            }
+
+            function updateDay(days) {
+                if (t.days <= 0) {
+                    t.days = '';
+                } else {
+                    return t.days;
+                }
+            }
+
+            function changeWord(word) {
+                if (t.days == 4 || t.days == 3 || t.days == 2) {
+                    return word.textContent = "дня";
+                } else if (t.days == 1) {
+                    return word.textContent = "день";
+                } else if (t.days < 1) {
+                    return word.textContent = '';
+                } else {
+                    return word.textContent = "дней";
+                }
+            }
+
+
+            hours.textContent = addZero(t.hours); //  поле часы
+            minutes.textContent = addZero(t.minutes); // field minutes
+            seconds.textContent = addZero(t.seconds); // field seconds
+            days.textContent = updateDay(t.days); // field days
+            word.textContent = changeWord(word);
+
+            // 
+            //
+            //если дата прошла выводим нули
+            if (t.total <= 0) {
+                clearInterval(timeInterval);
+                hours.textContent = '00';
+                minutes.textContent = '00';
+                seconds.textContent = '00';
+                days.textContent = '';
+                word.textContent = '';
+
+            }
+        }
+    }
+    // вызываем 
+    setClock('timer', deadline);
+
+	// Modal window
+
+	 // our variables
+	let more = document.querySelector('.more'),
+		overlay = document.querySelector('.overlay'),
+		close = document.querySelector('.popup-close');
+
+	// create our window
+	more.addEventListener('click', ()=>{
+		overlay.style.display = 'block';   // window has appear style after click
+		more.classList.add('more-splash'); //add class animation for this button 
+		document.body.style.overflow = 'hidden'; // stop scroll site page
+	});
+
+	// close our window
+	close.addEventListener('click', ()=>{
+		overlay.style.display = 'none';    // button has block style after click
+		more.classList.remove('more-splash'); // remove classList animation for this button 
+		document.body.style.overflow = '';  // can scroll page
+	});
+
+
+	// if you want to cancel to scroll page add this code row
+	// document.body.style.overflow = 'hidden'; // stop scroll site page
+	// document.body.style.overflow = '';  // can scroll page
+
+
 });
